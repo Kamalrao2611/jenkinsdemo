cc528043942ff9a335f2c87d35d9e8776aff489c
Kamal <kamalrao2611@gmail.com>
2024-01-11 11:00:53 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringDbAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbAwsApplication.class, args);
+	}
+
+}
